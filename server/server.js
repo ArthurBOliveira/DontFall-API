465c1116a3f65cb1ca4848d2549d8b000720378d
ArthurBOliveira <arthurboliveira93@gmail.com>
2017-09-28 00:36:56 -0300

    Added broadcast 'new user alert' message;

--- server/server.js
+++ server/server.js
@@ -12,19 +12,38 @@ let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) =>{
+io.on('connection', (socket) => {
     console.log('new user connect');
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Falaew Lek!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'Um Lek entrou!',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message) => {
         console.log(message);
+
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
-    socket.on('disconnect', () =>{
+    socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 });
